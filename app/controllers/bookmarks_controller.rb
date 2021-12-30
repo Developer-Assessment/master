4005ff08da0fc77c1179aa5c2433ae71ebbfbeae
Developer-Assessment <reva717@gmail.com>
2021-12-30 17:14:52 +0530

    Worked on review comments

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -50,15 +50,4 @@ class BookmarksController < ApplicationController
       params.require(:bookmark).permit(:title, :url)
     end
 
-    def authenticate_user
-      # find the user based on the headers from HTTP request
-      @current_user = User.find_by(
-        username: request.headers['X-Username'],
-        authentication_token: request.headers['X-Token']
-      )
-      
-      # return error message with 403 HTTP status if there's no such user
-      render(json: { message: 'Invalid User' }, status: 403) unless @current_user
-    end
-
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,4 @@
 class Bookmark < ApplicationRecord
-validates :title, presence: true, allow_blank: false
-validates :url, presence: true, allow_blank: false
+validates :title, presence: true
+validates :url, presence: true
 end
